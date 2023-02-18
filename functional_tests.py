8a35851867016e551bcbfe5f323e8eea912eecca
Vikhyat Sharma <vikhyatsharma1701@gmail.com>
2023-02-18 18:57:27 +0530

    Update functional tests to now check to-do list item input

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,11 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 import unittest
+import time
 
 # chromeService = Service("static/chromedriver.exe")
 # browser = webdriver.Chrome(service=chromeService)
@@ -29,15 +32,44 @@ class NewVisitorTest(unittest.TestCase):
 
         # I check the page title and that it mentions To-do lists
         self.assertIn('To-Do', self.browser.title)
+        headerText = self.browser.find_element(
+            By.TAG_NAME,
+            'h1'
+        ).text
+        self.assertIn('To-Do', headerText)
+
+        # I get an option to add a new list item after I open the page
+        inputBox = self.browser.find_element(
+            By.ID,
+            'id_new_item'
+        )
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter a To-Do item'
+        )
+
+        # I write 'Watch some interesting anime' into a text box
+        inputBox.send_keys("Watch some interesting anime")
+
+        # After hitting enter, the list gets updated and shows the entered item
+        inputBox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(
+            By.ID,
+            'id_list_table'
+        )
+        rows = table.find_elements(
+            By.TAG_NAME,
+            'tr'
+        )
+        self.assertTrue(
+            any(rows.text == "1: Watch some interesting anime")
+        )
+
+
         self.fail("Complete the test")
 
-
-
-# I get an option to add a new list item after I open the page
-
-# I write 'Watch some interesting anime' into a text box
-# 
-# After hitting enter, the list gets updated and shows the entered item
 
 # I still get an option to add another item and add other item "Watch Steins;Gate 0 anime"
 
