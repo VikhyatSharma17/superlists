d5fec4e361371ecfe9e4f1e6e7d3851de7be1790
Vikhyat Sharma <vikhyatsharma1701@gmail.com>
2023-03-13 23:54:52 +0530

    split the unit tests into different files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 
-from lists.views import home_page
 from lists.models import Item, List
 
 
@@ -9,39 +8,6 @@ class HomePage(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get(path='/')
         self.assertTemplateUsed(response=response, template_name='lists/home.html')
-
-
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'The second list item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_lists = List.objects.all()
-        self.assertEqual(saved_lists.count(), 1)
-
-        saved_list = saved_lists[0]
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "The second list item")
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class NewListTest(TestCase):
